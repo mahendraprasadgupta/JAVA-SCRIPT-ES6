9f228252bca4383865d9f9ed6e3b670528e44992
Mahendra Prasad Gupta <mpgupta1993@gmail.com>
2021-08-20 15:35:31 +0530

    Map Objects

---
+++ ES6/Map.js
@@ -0,0 +1,22 @@
+// JavaScript Map Objects
+// Being able to use an Object as a key is an important Map feature.
+
+
+
+// Create Objects
+const apples = {name: 'Apples'};
+const bananas = {name: 'Bananas'};
+const oranges = {name: 'Oranges'};
+
+console.log(apples);
+console.log(bananas);
+console.log(oranges);
+// Create a new Map
+const fruits = new Map();
+
+// Add new Elements to the Map
+fruits.set(apples, 500);
+fruits.set(bananas, 300);
+fruits.set(oranges, 200);
+
+console.log(fruits);
