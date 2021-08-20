aff628071b2fd2976de16e4ea47a7b31b162e8ad
Mahendra Prasad Gupta <mpgupta1993@gmail.com>
2021-08-20 15:41:31 +0530

    Set Object

---
+++ ES6/Set.js
@@ -0,0 +1,17 @@
+// JavaScript Set Objects
+
+// A Set is a collection of unique values.
+
+// Each value may occur only once in a Set.
+
+// A Set can hold any values of any data type.
+
+// Create a Set
+const letters = new Set();
+
+// Add some values to the Set
+letters.add("a");
+letters.add("b");
+letters.add("c");
+
+console.log(letters);
