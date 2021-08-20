eb4e559b760ed2c720e3e70fec8360920d1b646b
Mahendra Prasad Gupta <mpgupta1993@gmail.com>
2021-08-20 15:11:19 +0530

    const keyword

---
+++ ES6/const.js
@@ -0,0 +1,15 @@
+// JavaScript const
+// The const keyword allows you to declare a constant (a JavaScript variable with a constant value).
+
+// Constants are similar to let variables, except that the value cannot be changed.
+
+var x = 10;
+// Here x is 10
+console.log(x);
+{
+  const x = 2;
+  // Here x is 2
+  console.log(x);
+}
+// Here x is 10
+console.log(x);
