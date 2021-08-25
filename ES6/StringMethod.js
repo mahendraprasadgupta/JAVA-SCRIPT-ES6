eff0272a0571543ada7ea34b97395da98488ade0
Mahendra Prasad Gupta <mpgupta1993@gmail.com>
2021-08-26 00:03:03 +0530

    String function 2 .String.startsWith()

--- ES6/StringMethod.js
+++ ES6/StringMethod.js
@@ -1,7 +1,17 @@
-// String.includes()
+// String Methods
+
+// 1. String.includes()
 // The includes() method returns true if a string contains a specified value, otherwise false:
 // This Example
 
 let text = "Hello world, welcome to the universe.";
 text.includes("world")    // Returns true
 console.log(text.includes("world") )
+
+// 2. String.startsWith()
+// The startsWith() method returns true if a string begins with a specified value, otherwise false:
+
+let text1 = "Hello world, welcome to the universe.";
+
+text.startsWith("Hello")   // Returns true
+console.log(text1.startsWith("Hello"))
