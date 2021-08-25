d36db89866fc4e08d303d08f28624757ccf9973d
Mahendra Prasad Gupta <mpgupta1993@gmail.com>
2021-08-25 23:07:18 +0530

    String function 1.String.includes()

---
+++ ES6/StringMethod.js
@@ -0,0 +1,6 @@
+// String.includes()
+// The includes() method returns true if a string contains a specified value, otherwise false:
+
+let text = "Hello world, welcome to the universe.";
+text.includes("world")    // Returns true
+console.log(text.includes("world") )
